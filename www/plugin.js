3bc0a239870a508e46cbfed7d8a2024545f9b660
Matthew Lee <math.lee@qq.com>
2017-01-13 01:14:05 +0800

    added the Official Alipay SDK :kissing_heart:.

--- www/plugin.js
+++ www/plugin.js
@@ -5,10 +5,17 @@ var PLUGIN_NAME = 'MathPay';
 
 var MathPay = {
   echo: function(phrase, cb) {
-    exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
+      exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
   },
   getDate: function(cb) {
-    exec(cb, null, PLUGIN_NAME, 'getDate', []);
+	  exec(cb, null, PLUGIN_NAME, 'getDate', []);
+  },
+  alipay: function (paymentInfo, successCallback, errorCallback) {   
+    if(!paymentInfo){
+        errorCallback && errorCallback("Please enter order information");  
+    }else{
+        exec(successCallback, errorCallback, "AliPay", "pay", [paymentInfo]);
+    }
   }
 };
 
